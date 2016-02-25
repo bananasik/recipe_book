d878b2d19d5f1b9208bc4873ca1e4f3db39e88f2
KatarinaLenskaya <katarina.lenskaya@gmail.com>
2016-02-25 17:09:10 +0200

    Реализация диалогов для добавления разделов

--- RecipeCollection/app/src/main/java/Model/Section.java
+++ RecipeCollection/app/src/main/java/Model/Section.java
@@ -7,6 +7,11 @@ public class Section {
     public int id;
     public String name;
 
+    public Section(String _name){
+        id = 0;
+        name = _name;
+    }
+
     public Section(int _id, String _name){
         id = _id;
         name = _name;
